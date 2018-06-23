e8497879412b72df31208f62ebcdb425f4ed30ee
Oleg Gorodnitchi <oleg.gorodnitchi@sk.ibm.com>
2018-06-23 19:41:48 +0200

    iptables issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -53,11 +53,12 @@ OVPN_NETWORK_ROUTE=$(getroute ${OVPN_NETWORK})
 
 envsubst < $OVPN_TEMPLATE > $OVPN_CONFIG
 
-if [ $OVPN_DEFROUTE -gt 0 ]; then
-    iptables -t nat -C POSTROUTING -s $OVPN_NETWORK -o $OVPN_NATDEVICE -j MASQUERADE || {
-      iptables -t nat -A POSTROUTING -s $OVPN_NETWORK -o $OVPN_NATDEVICE -j MASQUERADE
-    }
-fi
+#if [ $OVPN_DEFROUTE -gt 0 ]; then
+#    iptables -t nat -C POSTROUTING -s $OVPN_NETWORK -o $OVPN_NATDEVICE -j MASQUERADE || {
+#      iptables -t nat -A POSTROUTING -s $OVPN_NETWORK -o $OVPN_NATDEVICE -j MASQUERADE
+#    }
+#fi
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 # Use client configuration directory if it exists.
 if [ -d "$OVPN_CCD" ]; then
